94a9f8033cf51c943c7f1845637a279cabac8063
Rahul Rudra <rahulrudra665@gmail.com>
2023-02-05 11:19:00 +0600

    new things changed

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const buttonClick=document.getElementById("buttonid").addEventListener("click",()=>{
     console.log("click");
-    const textValue=document.getElementById("text");
-    console.log(textValue.value);
+    const textValue=document.getElementById("text").value;
+    const mainCointainer=document.getElementById("maincontainer");
+
+    const paragragh=document.createElement("p");
+    const h1=document.createElement("h2");
+    const section=document.createElement("section");
+    section.innerHTML=`
+    <h2>Comment</h2>
+    <p>Rahul Rudra</p>
+    `
+mainCointainer.appendChild(section);
+
 })
