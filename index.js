22003b583a09f67ff1d7645b4cbf52d2f65e1eec
Rahul Rudra <rahulrudra665@gmail.com>
2023-02-05 11:08:36 +0600

    messages commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const buttonClick=document.getElementById("buttonid").addEventListener("click",()=>{
+    console.log("click");
+    const textValue=document.getElementById("text");
+    console.log(textValue.value);
+})
